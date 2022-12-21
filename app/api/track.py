c36787e779bb0823c5e0165c1e71d9f941a25cf9
geoffrey45 <geoffreymungai45@gmail.com>
2022-12-21 20:22:35 +0300

    revert from streaming file to send_file

--- app/api/track.py
+++ app/api/track.py
@@ -1,21 +1,10 @@
 """
 Contains all the track routes.
 """
-from flask import Blueprint, Response
+from flask import Blueprint, send_file
 from app.db.store import Store
 
 trackbp = Blueprint("track", __name__, url_prefix="/")
-
-
-def stream_file(filepath: str):
-    """
-    Streams a file to the client.
-    """
-    with open(filepath, "rb") as file:
-        data = file.read(1024)
-        while data:
-            yield data
-            data = file.read(1024)
 
 
 @trackbp.route("/file/<trackhash>")
@@ -37,4 +26,8 @@ def send_track_file(trackhash: str):
         return msg, 404
 
     audio_type = track.filepath.rsplit(".", maxsplit=1)[-1]
-    return Response(stream_file(track.filepath), mimetype=f"audio/{audio_type}")
+
+    try:
+        return send_file(track.filepath, mimetype=f"audio/{audio_type}")
+    except FileNotFoundError:
+        return msg, 404
