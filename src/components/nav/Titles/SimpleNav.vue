04ed6fd10e422ed0415ceef62c808de922fe83db
geoffrey45 <geoffreymungai45@gmail.com>
2023-01-13 18:47:32 +0300

    remove server code and replace it with client code

---
+++ src/components/nav/Titles/SimpleNav.vue
@@ -0,0 +1,9 @@
+<template>
+  <h2 style="margin: 0">{{ text }}</h2>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  text: string;
+}>();
+</script>
---
+++ src/utils/index.ts
@@ -0,0 +1,21 @@
+import createSubPaths from "./useCreateSubPaths";
+import focusElemByClass from "./useFocusElem";
+import formatSeconds from "./useFormatSeconds";
+import useFuse from "./useFuse";
+import { readLocalStorage, writeLocalStorage } from "./useLocalStorage";
+import putCommas from "./usePutCommas";
+import useVisibility from "./useVisibility";
+import useCreateTrackProps from "./useCreateTrackProps";
+
+export {
+    readLocalStorage,
+    writeLocalStorage,
+    createSubPaths,
+    focusElemByClass,
+    useVisibility,
+    formatSeconds,
+    useCreateTrackProps as createTrackProps,
+    putCommas,
+    useFuse,
+};
+
