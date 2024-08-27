bc2152c45c21b510812a34fa6c24466d87ee854e
Simonh2o <simon@mkdir.no>
2024-08-27 02:41:17 +0200

    fixed spacing, e.g properly lined up generic titles with artist headers etc

--- src/components/nav/NavButtons.vue
+++ src/components/nav/NavButtons.vue
@@ -1,49 +1,50 @@
 <template>
-  <div id="back-forward" class="">
-    <button class="back" @click="$router.back()">
-      <ArrowSvg />
-    </button>
-    <button class="forward" @click="$router.forward()">
-      <ArrowSvg />
-    </button>
-  </div>
+    <div id="back-forward" class="">
+        <button class="back" @click="$router.back()">
+            <ArrowSvg />
+        </button>
+        <button class="forward" @click="$router.forward()">
+            <ArrowSvg />
+        </button>
+    </div>
 </template>
 
 <script setup lang="ts">
-import ArrowSvg from "../../assets/icons/right-arrow.svg";
+import ArrowSvg from '../../assets/icons/right-arrow.svg'
 </script>
 
 <style lang="scss">
 #back-forward {
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  gap: 1rem;
-  padding-right: 1rem;
-  border-right: 1px solid $gray5;
-  height: max-content;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 1rem;
+    padding-right: 1rem;
+    border-right: 1px solid $gray5;
+    height: max-content;
 
-  & > * {
-    width: 2.25rem;
-    height: 2.25rem;
-    aspect-ratio: 1;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding: 0;
-    border-radius: 5rem;
+    & > * {
+        width: 2.25rem;
+        height: 2.25rem;
+        aspect-ratio: 1;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 0;
+        border-radius: 5rem;
+        background-color: $gray5;
 
-    svg {
-      transform: scale(1.12);
-      transition: transform 0.2s ease;
+        svg {
+            transform: scale(1.12);
+            transition: transform 0.2s ease;
 
-      &:active {
-        transform: scale(0.88);
-      }
+            &:active {
+                transform: scale(0.88);
+            }
+        }
     }
-  }
 
-  .back {
-    transform: rotate(180deg);
-  }
+    .back {
+        transform: rotate(180deg);
+    }
 }
 </style>
--- src/components/shared/GenericHeader.vue
+++ src/components/shared/GenericHeader.vue
@@ -14,18 +14,18 @@
 
 <style lang="scss">
 .generichead {
-  padding: 1rem 0 1rem $medium;
-  height: max-content;
-  display: grid;
-  grid-template-columns: 1fr max-content;
-  align-items: center;
-  max-width: 100%;
-  overflow: hidden;
-
-  .left {
+    padding: 0 0 1rem $medium;
+    height: max-content;
+    display: grid;
+    grid-template-columns: 1fr max-content;
+    align-items: center;
     max-width: 100%;
     overflow: hidden;
-  }
+
+    .left {
+        max-width: 100%;
+        overflow: hidden;
+    }
 
     h1 {
         width: max-content;
@@ -33,23 +33,23 @@
         font-size: 3.25rem;
     }
 
-  .title {
-    font-weight: 700;
-    margin-left: -1px;
-    max-width: 100%;
-    overflow: hidden;
-    text-overflow: ellipsis;
-  }
+    .title {
+        font-weight: 700;
+        margin-left: -1px;
+        max-width: 100%;
+        overflow: hidden;
+        text-overflow: ellipsis;
+    }
 
-  .desc {
-    font-size: 1rem;
-    line-height: 1.5;
-    font-weight: 500;
-  }
+    .desc {
+        font-size: 1rem;
+        line-height: 1.5;
+        font-weight: 500;
+    }
 
-  @include mediumPhones {
-    gap: 1rem;
-    grid-template-columns: repeat(auto-fill, 100%);
-  }
+    @include mediumPhones {
+        gap: 1rem;
+        grid-template-columns: repeat(auto-fill, 100%);
+    }
 }
 </style>
