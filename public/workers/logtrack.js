9b938194a60643b238f0545c2470609ca3460a92
cwilvx <geoffreymungai45@gmail.com>
2024-11-24 18:08:54 +0300

    fix: logtrack worker url formation

--- public/workers/logtrack.js
+++ public/workers/logtrack.js
@@ -2,7 +2,8 @@ onmessage = (e) => {
   const { trackhash, duration, source, timestamp } = e.data;
 
   const is_dev = location.port === "5173";
-  const base_url = is_dev ? "http://localhost:1980" : location.origin;
+  const protocol = location.protocol.replace(':', '');
+  const base_url = is_dev ? `${protocol}://${location.hostname}:1980` : location.origin;
   const url = base_url + "/logger/track/log";
 
   fetch(url, {
